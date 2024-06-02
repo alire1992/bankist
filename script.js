83044e086a1abcbba51a2a1b41c41f93096977fd
alireza saedi <75143302+alire1992@users.noreply.github.com>
2024-06-02 17:50:22 +0330

    Build a Tabbed Component

--- script.js
+++ script.js
@@ -68,3 +68,31 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Tabed components
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+  if (clicked.classList.contains("operations__tab")) {
+    tabs.forEach((tab) => {
+      tab.classList.remove("operations__tab--active");
+      if (tab.classList.contains(`operations__tab--${clicked.dataset.tab}`)) {
+        tab.classList.add("operations__tab--active");
+      }
+    });
+    tabsContent.forEach((tabContent) => {
+      tabContent.classList.remove("operations__content--active");
+      if (
+        tabContent.classList.contains(
+          `operations__content--${clicked.dataset.tab}`
+        )
+      ) {
+        tabContent.classList.add("operations__content--active");
+      }
+    });
+  }
+});
