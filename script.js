ccb3509891fc5d3f507a78387e22432b7ff94164
alireza saedi <75143302+alire1992@users.noreply.github.com>
2024-06-03 11:25:22 +0330

    Implement blur effect on navigation bar

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 "use strict";
 
 ///////////////////////////////////////
-// Modal window
-
+const nav = document.querySelector(".nav");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -31,9 +36,6 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
-
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
 
 // Button scrolling
 btnScrollTo.addEventListener("click", function () {
@@ -70,10 +72,6 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 });
 
 // Tabed components
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
   if (!clicked) return;
@@ -96,3 +94,22 @@ tabsContainer.addEventListener("click", function (e) {
     });
   }
 });
+
+// Menu fade animation
+function handleHover(e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+    siblings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = this;
+        logo.style.opacity = this;
+      }
+    });
+  }
+}
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+// Sticky navigation
